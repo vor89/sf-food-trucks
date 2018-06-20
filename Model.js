3ec714416162d4d6688a5075b739287fe4207228
Ryan Vo <ryan@Ryans-MBP.T-mobile.com>
2018-06-20 15:39:42 -0700

    Updated models to handle non-200 responses; improved query

--- Model.js
+++ Model.js
@@ -1,17 +1,28 @@
 const request = require('request');
 
-const getFoodTrucks = function (time, offset, limit) {
-  const url = `http://data.sfgov.org/resource/bbb8-hzi6.json?$where=start24 <= "${time}" and "${time}" < end24 &$offset= ${offset} &$limit= ${limit}`;
+const getFoodTrucks = function (day, time, offset, limit) {
+  const dayBefore = day === 0 ? 6 : day - 1;
+  const baseURL = 'http://data.sfgov.org/resource/bbb8-hzi6.json';
+  const query = `$select= applicant, location, dayorder, start24, end24
+    &$where= (dayorder = ${day} and start24 <= "${time}" and "${time}" < end24)
+      or (end24 < start24 and dayorder = ${day} and start24 <= "${time}")
+      or (end24 < start24 and dayorder = ${dayBefore} and "${time}" < end24)
+    &$order= applicant, location
+    &$offset= ${offset}
+    &$limit= ${limit}`;
+  const fullURL = [baseURL, '?', query].join('');
   return new Promise((resolve, reject) => {
-    request(url, (error, response, body) => {
+    request(fullURL, (error, response, body) => {
       if (error) {
         reject(error);
+      } else if (response && response.statusCode !== 200) {
+        reject(body);
       } else {
-        resolve(body);
+        resolve(JSON.parse(body));
       }
     });
   });
 }
 
 // TODO: use babel to use import/export
-exports.default = getFoodTrucks;
+exports.getFoodTrucks = getFoodTrucks;
